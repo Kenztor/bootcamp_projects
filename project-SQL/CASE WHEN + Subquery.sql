28c20521ea96e9c1e77b012ec9eae9b9b07c2ea1
Kenztor <72817450+Kenztor@users.noreply.github.com>
2023-02-13 23:51:51 +0700

    Create subqueries + WITH.sql

--- project-SQL/CASE WHEN + Subquery.sql
+++ project-SQL/CASE WHEN + Subquery.sql
@@ -5,13 +5,13 @@ SELECT
 	COUNT(*) AS n_sub
 FROM (
 	SELECT 
-		country,
-    CASE 
-    	WHEN country IN ('USA', 'Canada') THEN 'North America'
-			WHEN country IN ('Brazil') THEN 'South America'
-      WHEN country IN ('Italy', 'Belgium', 'Germany', 'Denmark', 'Portugal') THEN 'Europe'
-    	ELSE 'Other Regions'
-    END AS region
+	   country,
+    	   CASE 
+    	    WHEN country IN ('USA', 'Canada') THEN 'North America'
+	    WHEN country IN ('Brazil') THEN 'South America'
+            WHEN country IN ('Italy', 'Belgium', 'Germany', 'Denmark', 'Portugal') THEN 'Europe'
+    	    ELSE 'Other Regions'
+           END AS region
 	FROM customers
 ) AS sub
 GROUP BY 1
@@ -20,19 +20,19 @@ ORDER BY 2 DESC -- descending order high to low
 -- refactor using WITH clause
 WITH sub AS (
 	SELECT 
-		country,
-    CASE 
-    	WHEN country IN ('USA', 'Canada') THEN 'North America'
-			WHEN country IN ('Brazil') THEN 'South America'
-      WHEN country IN ('Italy', 'Belgium', 'Germany', 'Denmark', 'Portugal') THEN 'Europe'
-    	ELSE 'Other Regions'
-    END AS region
+	   country,
+           CASE 
+    	    WHEN country IN ('USA', 'Canada') THEN 'North America'
+	    WHEN country IN ('Brazil') THEN 'South America'
+            WHEN country IN ('Italy', 'Belgium', 'Germany', 'Denmark', 'Portugal') THEN 'Europe'
+    	    ELSE 'Other Regions'
+           END AS region
 	FROM customers
 )
 
 SELECT 
-	region,
-	COUNT(*) AS n_sub
+   region,
+   COUNT(*) AS n_sub
 FROM sub
 GROUP BY 1
 ORDER BY 2 DESC
